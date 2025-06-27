0dd5d4f55abb8b22c2b9d67647204e3e428f5f4f
AnHaiDung <anhaidung2k6@gmail.com>
2025-06-27 15:40:13 +0700

    Add files via upload

--- PTIT_CNTT2_IT201_Session06_Bai05/main.c
+++ PTIT_CNTT2_IT201_Session06_Bai05/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int maxMin(int arr[],int len,int *max,int *min,int n) {
-    if(n>=len) return 0;
-    if (*max < arr[n]) {
-        *max = arr[n];
+int maxMin(int arr[],int len,int max,int min,int n) {
+    if(n>=len) {
+        printf("max = %d, min = %d\n", max, min);
+        return 0;
     }
-    if (*min > arr[n] ) {
-        *min = arr[n];
+    if (max < arr[n]) {
+        max = arr[n];
+    }
+    if (min > arr[n] ) {
+        min = arr[n];
     }
     return maxMin(arr,len,max,min,n+1);
 }
@@ -20,9 +23,6 @@ int main(void) {
         printf("nhap phan tu thu %d: ",i);
         scanf("%d", &arr[i]);
     }
-    int max = arr[0];
-    int min = arr[0];
-    maxMin(arr, len,&max,&min , 0);
-    printf("max = %d, min = %d\n", max, min);
+    maxMin(arr, len,arr[0],arr[0] , 0);
     return 0;
 }
